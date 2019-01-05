3c2ae7319856eddc3a37bf16ec735b33f37ec562
mail4hafij <mail4hafij@yahoo.com>
2019-01-05 16:09:40 +0100

    ready to start php MVC project

--- application/layout/layout.php
+++ application/layout/layout.php
@@ -20,14 +20,9 @@
       <div class="clb"></div>
 
       <div id="footwrap">&copy; Free to use phpwebpad</div>
-      <div class="white">
-        <i><strong>
-        php framework, php mvc framework, opensource php framework,
-        simple php framework, easy php framework, simple orm, php orm,
-        php mysql orm, php-mysql orm, easy orm, opensource php orm,
-        open source, phpwebpad, easy, mvc, orm, model, view, controller
-        </strong></i>
-      </div>
+      
+      <?php echo Controller::renderElement("layout/keywords"); ?>
+      
     </div>
   </body>
 </html>
